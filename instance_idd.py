2a1878a9d17bd8973789fa26548687e42ac755ba
saket1919 <96956210+saket1919@users.noreply.github.com>
2022-05-24 16:56:23 +0530

    Add files via upload

---
+++ instance_idd.py
@@ -0,0 +1,22 @@
+import boto3
+region = 'ap-south-1'
+
+ec2 = boto3.client('ec2',
+                   'ap-south-1',
+                   aws_access_key_id='AKIA5H272UWO3LAE7HOS',
+                   aws_secret_access_key='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')
+
+def list_instances():
+    instance_ids = []
+    response = ec2.describe_instances(Filters=[{'Name': 'instance-type', 'Values': ["t2.micro", "t3.micro"]}])
+    instances_full_details = response['Reservations']
+    for instance_detail in instances_full_details:
+        group_instances = instance_detail['Instances']
+
+        for instance in group_instances:
+            instance_id = instance['InstanceId']
+            instance_ids.append(instance_id)
+    return instance_ids
+
+instance_ids = list_instances()
+print(instance_ids)

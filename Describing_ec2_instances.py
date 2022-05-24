bc7303152fccc0a788e01a58491ef80b0dedcfa7
saket1919 <96956210+saket1919@users.noreply.github.com>
2022-05-24 17:49:08 +0530

    Add files via upload

---
+++ Describing_ec2_instances.py
@@ -0,0 +1,31 @@
+import json
+from datetime import date, datetime
+import boto3
+
+AWS_REGION = "ap-south-1"
+EC2_CLIENT = boto3.client('ec2', region_name=AWS_REGION)
+INSTANCE_ID = ''
+
+
+# Helper method to serialize datetime fields
+def json_datetime_serializer(obj):
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError ("Type %s not serializable" % type(obj))
+
+
+response = EC2_CLIENT.describe_instances(
+    InstanceIds=[
+        INSTANCE_ID,
+    ],
+)
+
+print(f'Instance {INSTANCE_ID} attributes:')
+
+for reservation in response['Reservations']:
+    print(json.dumps(
+            reservation,
+            indent=4,
+            default=json_datetime_serializer
+        )
+    )
